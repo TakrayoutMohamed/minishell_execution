421620909495f820c4bc8835c5ff7aa7399b08c0
TakrayoutMohamed <takrayoutm@gmail.com>
2023-08-25 12:48:10 +0100

    testing the leaks has been passed successfully

--- testing/exit_tst.c
+++ testing/exit_tst.c
@@ -2,7 +2,7 @@
 
 void foo(void)
 {
-	system("leaks export");
+	system("leaks exit");
 }
 int main(int argc, char **argv, char **env)
 {
@@ -13,13 +13,13 @@ int main(int argc, char **argv, char **env)
 	cmd = NULL;
     t_list  *env_lst;
 
-	// atexit(foo);
+	atexit(foo);
     env_lst = convert_env_to_list(env);
 	// tmp = ft_lstnew(ft_strdup("NULL"), ft_strdup("NULL"));
 	// ft_lstadd_back(&lst, tmp);
 	ft_lstadd_back(&cmd, ft_lstnew(NULL, ft_strdup("exit")));
 	ft_lstadd_back(&cmd, ft_lstnew(NULL, ft_strdup("-922")));
-	ft_lstadd_back(&cmd, ft_lstnew(NULL, ft_strdup("-922")));
+	// ft_lstadd_back(&cmd, ft_lstnew(NULL, ft_strdup("-922")));
 
 
     exit_(cmd);
