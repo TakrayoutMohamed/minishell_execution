650359aeaf1abcbcd036635bc5704a4a8b88be24
TakrayoutMohamed <takrayoutm@gmail.com>
2023-09-03 18:02:10 +0100

    the beginning pipe is good as pipe i still need to handle the exit status

--- pipex/pipe_beginning.c
+++ pipex/pipe_beginning.c
@@ -6,7 +6,7 @@
 /*   By: takra <takra@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 18:49:33 by takra             #+#    #+#             */
-/*   Updated: 2023/09/03 05:07:22 by takra            ###   ########.fr       */
+/*   Updated: 2023/09/03 18:01:28 by takra            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,19 @@
 
 static void	pipe_beginning_infile(t_list *lst)
 {
+	t_list	*tmp;
+
+	tmp = lst;
 	if (lst->infile > 0)
 	{
 		dup2(lst->infile, 0);
 		close(lst->infile);
 	}
-	else
+	close(lst->pipe[0]);
+	while (tmp->next != NULL)
 	{
-		// dup2(lst->pipe[0], 0);
+		close(tmp->pipe[0]);
+		tmp = tmp->next;
 	}
 }
 
@@ -36,14 +41,13 @@ static void	pipe_beginning_outfile(t_list *lst)
 	{
 		dup2(lst->pipe[1], 1);
 	}
+	close(lst->pipe[1]);
 }
 
 int	pipe_beginning(t_list *lst, char **argv, char **envp)
 {
 	pid_t	pid;
-	t_list	*tmp;
 
-	tmp = lst;
 	pid = fork();
 	if (pid == -1)
 	{
@@ -54,18 +58,6 @@ int	pipe_beginning(t_list *lst, char **argv, char **envp)
 	{
 		pipe_beginning_infile(lst);
 		pipe_beginning_outfile(lst);
-		close(lst->pipe[0]);
-		close(lst->pipe[1]);
-		while (tmp->next != NULL)
-		{
-			close(tmp->pipe[0]);
-			tmp = tmp->next;
-		}
-		// while (tmp->previous != NULL)
-		// {
-		// 	close(tmp->previous->pipe[0]);
-		// 	tmp = tmp->previous;
-		// }
 		execve(argv[0], argv, envp);
 		t_stats.status = errno;
 		exit(t_stats.status);
