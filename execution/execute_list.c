9b38ebe7b202e59bd0ce4dd03c19b7e358b4096b
TakrayoutMohamed <takrayoutm@gmail.com>
2023-09-03 19:19:10 +0100

    exit status so far working perfectlly

--- execution/execute_list.c
+++ execution/execute_list.c
@@ -6,7 +6,7 @@
 /*   By: takra <takra@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 18:49:08 by takra             #+#    #+#             */
-/*   Updated: 2023/09/03 04:23:42 by takra            ###   ########.fr       */
+/*   Updated: 2023/09/03 19:18:04 by takra            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,46 +28,68 @@ static bool	is_output_builtins(t_list *lst_cmd, char *str)
 int	wait_allchilds(void)
 {
 	int	st;
-	int	i;
-
-	i = 1;
+	int	st1;
+	
+	st1 = t_stats.status;
 	while (waitpid(-1, &st, 0) != -1)
 	{
-		fprintf(stderr, "here is the process %d\n", i);
-		i++;
+		st1 = st;
 	}
-	return (st);
+	return (st1);
 }
 
-void	close_all_pipes(t_list *lst)
-{
-	t_list	*tmp;
-	int		i;
+// void	close_all_pipes(t_list *lst)
+// {
+// 	t_list	*tmp;
+// 	int		i;
 
-	i = 1;
-	tmp = lst;
-	while (tmp->previous != NULL)
-		tmp = tmp->previous;
-	while (tmp)
-	{
-		fprintf(stderr, "==================\n");
-		fprintf(stderr, "the lst value = %d\n", i);
-		fprintf(stderr, "input\n");
-		if (close(tmp->pipe[0]) == -1)
-		{
-			print_error(errno);
-			fprintf(stderr, " \n");
-		}
-		fprintf(stderr, "output\n");
-		if (close(tmp->pipe[1]) == -1)
-		{
-			print_error(errno);
-			fprintf(stderr, " \n");
-		}
-		fprintf(stderr, "==================\n");
-		i++;
-		tmp = tmp->next;
-	}
+// 	i = 1;
+// 	tmp = lst;
+// 	while (tmp->previous != NULL)
+// 		tmp = tmp->previous;
+// 	while (tmp)
+// 	{
+// 		fprintf(stderr, "==================\n");
+// 		fprintf(stderr, "the lst value = %d\n", i);
+// 		fprintf(stderr, "input\n");
+// 		if (close(tmp->pipe[0]) == -1)
+// 		{
+// 			print_error(errno);
+// 			fprintf(stderr, " \n");
+// 		}
+// 		fprintf(stderr, "output\n");
+// 		if (close(tmp->pipe[1]) == -1)
+// 		{
+// 			print_error(errno);
+// 			fprintf(stderr, " \n");
+// 		}
+// 		fprintf(stderr, "==================\n");
+// 		i++;
+// 		tmp = tmp->next;
+// 	}
+// }
+
+
+int	get_position(t_list *lst, t_list *tmp)
+{
+	int	position;
+
+	position = 0;
+	if (tmp == ft_lstlast(lst))
+		position = 3;
+	else if (tmp == lst)
+		position = 1;
+	else
+		position = 2;
+	return (position);
+}
+
+void	set_status(int status)
+{
+	if (WIFEXITED(status))
+		t_stats.status = WEXITSTATUS(status);
+	if (WIFSIGNALED(status))
+		t_stats.status = WTERMSIG(status) + 128;
 }
 
 /*
@@ -84,12 +106,7 @@ void	execute_list(t_list *lst, t_list *env)
 	tmp = lst;
 	while (tmp)
 	{
-		if (tmp == ft_lstlast(lst))
-			position = 3;
-		else if (tmp == lst)
-			position = 1;
-		else
-			position = 2;
+		position = get_position(lst, tmp);
 		if (is_builtins(tmp->cmd))
 		{
 			if (is_output_builtins(tmp->cmd, tmp->cmd->value))
@@ -101,6 +118,5 @@ void	execute_list(t_list *lst, t_list *env)
 			execution(tmp, env, position);
 		tmp = tmp->next;
 	}
-	close_all_pipes(lst);
-	t_stats.status = wait_allchilds();
+	set_status(wait_allchilds());
 }
