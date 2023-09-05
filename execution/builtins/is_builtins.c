a776017b19d30e09050b83ce3a9faf236751e1ce
TakrayoutMohamed <takrayoutm@gmail.com>
2023-09-05 21:36:06 +0100

    protect is_builtins

--- execution/builtins/is_builtins.c
+++ execution/builtins/is_builtins.c
@@ -6,7 +6,7 @@
 /*   By: takra <takra@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/05 22:43:00 by mohtakra          #+#    #+#             */
-/*   Updated: 2023/09/02 17:31:59 by takra            ###   ########.fr       */
+/*   Updated: 2023/09/05 21:34:27 by takra            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 /*return true if the first node's value is a builtins programe*/
 int	is_builtins(t_list *lst)
 {
+	if (!lst || !lst->value)
+		return (0);
 	if (is_echo(lst->value))
 		return (1);
 	if (is_cd(lst->value))
